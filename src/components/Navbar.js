aa8a0bbab4bd6f8b55deefb8950948147597a932
HobbesChiong <93816506+HobbesChiong@users.noreply.github.com>
2022-04-30 03:13:41 -0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,14 @@ function Navbar() {
   return (
     <header className="bg-green-300 sticky top-0 z-50 p-3 drop-shadow-md">
       
-      <div className=" overflow-x-auto overflow-y-hidden container mx-auto flex justify-between">
-        <nav className="my-0 relative sm:container mx-auto flex gap-3 sm:gap-10">
-          <div className="p-5 bg-blue-500 rounded-lg drop-shadow-md">
+      <div className=" overflow-x-auto overflow-y-hidden container mx-auto flex justify-center">
+      
+          <div className="p-5 mr-20 bg-blue-500 rounded-lg drop-shadow-md">
             <Link href="/">
               <a className="comic text-2xl font-bold text-white p-2 pl-20 pr-20 rounded-lg shadow-inner bg-orange-300 hover:bg-orange-400 "> About </a>
             </Link>
           </div>
-          <div className="p-5 bg-blue-500 rounded-lg drop-shadow-md">
+          <div className="p-5 mr-20 bg-blue-500 rounded-lg drop-shadow-md">
             <Link href="/Recipes">
               <a className="comic text-2xl font-bold text-white p-2 pl-20 pr-20 rounded-lg shadow-inner bg-orange-300 hover:bg-orange-400 ">Recipes</a>
             </Link>
@@ -22,7 +22,7 @@ function Navbar() {
               <a className="comic text-2xl font-bold text-white p-2 pl-20 pr-20 rounded-lg shadow-inner bg-orange-300 hover:bg-orange-400 ">Create</a>
             </Link>
           </div>
-        </nav>
+
       </div>
     </header>
   );
