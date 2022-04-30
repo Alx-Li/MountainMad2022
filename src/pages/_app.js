bc46c0578c795dd7edc36216103eab5182ad3920
Alx-Li <li.alex.0901@gmail.com>
2022-04-30 04:15:03 -0700

    background fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import "../public/style/global.css";
 import Navbar from "../components/Navbar"
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <div className>
+    <div class="bg-[url('https://cdnb.artstation.com/p/assets/images/images/019/146/383/large/cartoonik-animation-cs-kitchen-bg.jpg?1562206094')] min-w-screen min-h-screen overflow-hidden bg-fixed bg-center bg-clip-content">
       <Navbar/>
       <Component {...pageProps}/>
     </div>
