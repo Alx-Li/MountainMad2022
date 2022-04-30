9d0844764fa207d9c0a7078fdf76c3302ff05f15
HobbesChiong <93816506+HobbesChiong@users.noreply.github.com>
2022-04-30 03:16:37 -0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,14 @@ function Navbar() {
   return (
     <header className="bg-green-300 sticky top-0 z-50 drop-shadow-md">
       
-      <div className=" overflow-x-auto overflow-y-hidden container mx-auto flex justify-center">
+      <div className=" overflow-x-auto p-5 overflow-y-hidden container mx-auto flex justify-center">
       
-          <div className="p-5 mr-20 bg-blue-500 rounded-lg drop-shadow-md">
+          <div className="p-5 mr-20 bg-blue-500 rounded-lg drop-shadow-3xl">
             <Link href="/">
               <a className="comic text-2xl font-bold text-white p-2 pl-20 pr-20 rounded-lg shadow-inner bg-orange-300 hover:bg-orange-400 "> About </a>
             </Link>
           </div>
-          <div className="p-5 mr-20 bg-blue-500 rounded-lg drop-shadow-md">
+          <div className="p-5 mr-20 bg-blue-500 rounded-lg drop-shadow-3xl">
             <Link href="/Recipes">
               <a className="comic text-2xl font-bold text-white p-2 pl-20 pr-20 rounded-lg shadow-inner bg-orange-300 hover:bg-orange-400 ">Recipes</a>
             </Link>
