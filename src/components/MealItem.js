4f26f59c423d4d8185fa0dcd761eefa751faa8ee
ransurf <ransomguest@gmail.com>
2022-04-30 02:53:13 -0700

    yep cock

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,16 +1,25 @@
 const MealItem = ({ meal }) => {
   // last commit
   return (
-    <div className="shadow-lg rounded-lg bg-orange-300 p-4">
-      <img src={meal.image} className="w-full h-auto" alt="Chicken Salad" />
-      <h3 className="text-xl font-normal text-center comic py-2">{meal.name}</h3>
-      <div className="">
-        <div className="px-3 font-light bg-white text-oragne-300 comic">
-          <p className="self-start italic pb-2 text-center">{meal.description}</p>
-          <p>Ingredients: <span className="italic">{meal.ingredients}</span></p>
-          <p>Cooking Method: {meal.method}</p>
-          <p className="">Made by: {meal.author}</p>
-        </div>
+    <div className="shadow-lg rounded-lg bg-white border-8 max-height-4 border-orange-300 p-4">
+      <div>
+        <img src={meal.image} className="w-full h-auto" alt="Chicken Salad" />
+      </div>
+
+      <div className="px-3 font-light bg-white text-oragne-300 comic">
+        <h3 className="text-xl font-normal text-center comic py-2">
+          {meal.name}
+        </h3>
+        <p className="self-start italic pb-2 text-center">{meal.description}</p>
+        <p>
+          Ingredients: <span className="italic">{meal.ingredients}</span>
+        </p>
+        <p>
+          Cooking Method: <span className="italic">{meal.method}</span>
+        </p>
+        <p className="">
+          Made by: <span className="italic">{meal.author}</span>
+        </p>
       </div>
     </div>
   );
